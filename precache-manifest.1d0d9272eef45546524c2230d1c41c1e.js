84d3ca94b63f622c282b8e7d3388d239a5cbcdc0
Jenna Goldman <jenna.goldman89@gmail.com>
2020-03-01 22:46:04 -0800

    Updates

---
+++ precache-manifest.1d0d9272eef45546524c2230d1c41c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e12e98252b903d543c4901852ece46",
+    "url": "/favorites-frontend/index.html"
+  },
+  {
+    "revision": "b0ce8119907be5e1bc03",
+    "url": "/favorites-frontend/static/css/main.d2903297.chunk.css"
+  },
+  {
+    "revision": "7a5fea0185f78ac17051",
+    "url": "/favorites-frontend/static/js/2.e1783d52.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/favorites-frontend/static/js/2.e1783d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ce8119907be5e1bc03",
+    "url": "/favorites-frontend/static/js/main.088f702c.chunk.js"
+  },
+  {
+    "revision": "989fe5f93a05e8ca5ad1",
+    "url": "/favorites-frontend/static/js/runtime-main.69fa3c42.js"
+  }
+]);
